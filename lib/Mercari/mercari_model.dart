cdc171decfe1e3bd93ace3a29411faa295ba159e
tbskgm0 <tsubasa.kogoma@axia-works.com>
2022-06-09 23:40:37 +0900

    add: メルカリアプリ実装

---
+++ lib/Mercari/mercari_model.dart
@@ -0,0 +1,14 @@
+
+class MercariItem {
+  String imageName;
+  String itemName;
+  int itemPrice;
+  int watching;
+
+  MercariItem(
+      this.imageName,
+      this.itemName,
+      this.itemPrice,
+      this.watching,
+      );
+}
